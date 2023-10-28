c2ee6a0557b672d0f47ebbeabdae034de15ba104
DNDK <dubovec.andrey.2005@gmail.com>
2023-10-28 15:07:08 +0300

    ???

--- newton/Gauss.cpp
+++ newton/Gauss.cpp
@@ -6,14 +6,14 @@ using namespace std;
 void Gauss(double** A, double* B, double* X, int N)
 {
 
-    for (int k = 0; k < N; k++) // ������ ���
+    for (int k = 0; k < N; k++)
     {
         for (int i = k + 1; i < N; i++)
         {
             if (abs(A[i][k]) > abs(A[k][k]))
             {
-                swap(A[i], A[k]);  //�������� ������ �.�. ��������� ������
-                swap(B[i], B[k]);   //�������� ��������
+                swap(A[i], A[k]);
+                swap(B[i], B[k]);   
             }
         }
         float A_Main = A[k][k];
@@ -37,7 +37,7 @@ void Gauss(double** A, double* B, double* X, int N)
             B[i] -= s * B[k];
         }
     }
-    for (int k = N - 1; k >= 0; k--)  //�������� ���
+    for (int k = N - 1; k >= 0; k--) 
     {
         X[k] = B[k];
         for (int i = N - 1; i > k; i--)
